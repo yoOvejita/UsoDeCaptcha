638e04dc56cae0faf5fd52663c2f08a2884ac665
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2020-05-28 20:08:44 -0400

    Commit unico

--- UsoDeCaptcha/Controllers/HomeController.cs
+++ UsoDeCaptcha/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using CaptchaMvc.HtmlHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UsoDeCaptcha.Models;
 
 namespace UsoDeCaptcha.Controllers
 {
@@ -25,6 +27,24 @@ namespace UsoDeCaptcha.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult EjemploCaptcha() {
+            Cuenta cuenta = new Cuenta();
+            return View("EjemploCaptcha", cuenta);
+        }
+        [HttpPost]
+        public ActionResult Registro(Cuenta cu) {
+            
+            if (!this.IsCaptchaValid(""))   //Primero verificamos si el captcha es correcto
+            {
+                ViewBag.error = "Captcha invalido";
+                return View("EjemploCaptcha", cu);//retornamos el objeto Cuenta (cu) para mostrar el formulario con todos los datos
+            }
+            if (!ModelState.IsValid)    //Luego verificamos si el modelo es correcto
+                return View("EjemploCaptcha", cu);
+            ViewBag.cuenta = cu;
+            return View("Success");     //Si todo es correcto retornamos la vista successs
+
         }
     }
 }
---
+++ UsoDeCaptcha/Models/Cuenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UsoDeCaptcha.Models
+{
+    public class Cuenta
+    {
+        [Required]
+        public string NombreUsuario { get; set; }
+        [Required]
+        public string Contraseña { get; set; }
+        [Required]
+        public string NombreCompleto { get; set; }
+    }
+}
